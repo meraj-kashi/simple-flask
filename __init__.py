0154faacaac318f63b0dd855ee518518c5d9cf17
meraj-kashi <meraj.mostamand.kashi@gmail.com>
2020-03-22 17:25:57 +0100

    Using requet to receive data from HTML Form by POST method

--- __init__.py
+++ __init__.py
@@ -3,7 +3,7 @@ import random
 
 app= Flask(__name__)
 
-@app.route('/')
+@app.route('/',methods=['POST','GET']) # read both GET nad POST method
 def hello_world():
     #Create a randome number between 0 and 1 to show on HTML page
     rand=random.randint(0,1)
@@ -13,6 +13,9 @@ def hello_world():
         var_name='False'
     
     #Get name from request arquemnt
-    name=request.args.get('name')  #exp: http://localhost:5000/?name=meraj
+    if not request.form.get('name'):  #If no name enetered 
+        name="guest"
+    else:
+        name=request.form.get('name')  #exp: http://localhost:5000/?name=meraj but here I use POST method! 
     
     return render_template("index.html",name=name,var_name=var_name )
