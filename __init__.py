212e0481195ade07e2fcc6efcea83c980ac4b402
meraj-kashi <meraj.mostamand.kashi@gmail.com>
2020-03-22 16:35:45 +0100

    Using request function from flask to read argument

--- __init__.py
+++ __init__.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 import random
 
 app= Flask(__name__)
 
 @app.route('/')
 def hello_world():
+    #Create a randome number between 0 and 1 to show on HTML page
     rand=random.randint(0,1)
     if rand==1 :
         var_name='True'
     else:
         var_name='False'
-
-    return render_template("index.html",var_name=var_name)
+    
+    #Get name from request arquemnt
+    name=request.args.get('name')  #exp: http://localhost:5000/?name=meraj
+    
+    return render_template("index.html",name=name,var_name=var_name )
